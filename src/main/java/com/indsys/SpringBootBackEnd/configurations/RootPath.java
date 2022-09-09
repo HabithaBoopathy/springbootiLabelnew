e21e584cad73c7defafbe301dbec14079da8027a
HabithaBoopathy <habithakkl@gmail.com>
2022-09-09 11:10:34 +0530

    first commit

---
+++ src/main/java/com/indsys/SpringBootBackEnd/configurations/RootPath.java
@@ -0,0 +1,14 @@
+package com.indsys.SpringBootBackEnd.configurations;
+
+public class RootPath {
+
+    //lightSail
+//    public static String rootPath = "/home/ubuntu/ilabel/uploads/";
+
+    //VM
+//    public static String rootPath = "/home/Projects/SpringBootBackEnd/uploads/";
+
+    //Local Machines
+    //Kannan
+    public static String rootPath = "E:/spring-boot/SpringBootBackEnd/uploads/";
+}
